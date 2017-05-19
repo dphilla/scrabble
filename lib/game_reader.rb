a7f1ce571c079b34881a861043613d66d6aa9196
Daniel Phillips <danieledward.phillips@gmail.com>
2017-05-19 11:54:55 -0600

    tested and wrote word scoring class, i.e. GameReader

--- lib/game_reader.rb
+++ lib/game_reader.rb
@@ -1,3 +1,32 @@
 class GameReader
+  attr_reader :scores
+
+  def initialize(scores = nil)
+    @scores = scores
+    @score_dict ={
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
+              }
+  end
+
+   def score(letter)
+    letter = letter.upcase
+    if letter.length > 1
+      letter = letter.split(//)
+      word_score = letter.map { |character| @score_dict[character]}
+      @score = word_score.reduce(:+)
+    else
+      @score = @score_dict[letter]
+    end
+  end
 
 end
+
+
+# instance = GameReader.new
+# require 'pry'; binding.pry
--- test/game_reader_test.rb
+++ test/game_reader_test.rb
@@ -1,9 +1,22 @@
 gem 'minitest'
-require_relative '../lib/game_reader'
+require './lib/game_reader'
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'pry'
 
 class GameReaderTest < Minitest::Test
 
+  def test_letters_have_scores
+    new_game = GameReader.new
+    expected = 1
+    result = new_game.score("A")
+    assert_equal expected, result
+  end
+
+  def test_can_check_words
+    new_game = GameReader.new
+    expected = 10
+    result = new_game.score("heyo")
+    assert_equal expected, result
+  end
 end
